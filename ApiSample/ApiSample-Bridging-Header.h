9c1d1020662e1426b1ce21ce19b54a311a4a24de
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2015-06-26 09:24:59 +0900

    Add AFNetwork

---
+++ ApiSample/ApiSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ApiSample-Bridging-Header.h
+//  ApiSample
+//
+//  Created by shinobu okano on 2015/06/26.
+//  Copyright (c) 2015年 shinobu okano. All rights reserved.
+//
+
+#ifndef ApiSample_ApiSample_Bridging_Header_h
+#define ApiSample_ApiSample_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#endif
--- ApiSample/ViewController.swift
+++ ApiSample/ViewController.swift
@@ -19,13 +19,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func load(sender: AnyObject) {
-        let url = NSURL(string: "http://express.heartrails.com/api/json?method=getPrefectures")
-        let urlRequest = NSURLRequest(URL: url!)
-        let connection: NSURLConnection = NSURLConnection(request: urlRequest, delegate: self, startImmediately: false)!
-
-        NSURLConnection.sendAsynchronousRequest(urlRequest,
-            queue: NSOperationQueue.mainQueue(),
-            completionHandler: response)
+//        let url = NSURL(string: "http://express.heartrails.com/api/json?method=getPrefectures")
+//        let urlRequest = NSURLRequest(URL: url!)
+//        let connection: NSURLConnection = NSURLConnection(request: urlRequest, delegate: self, startImmediately: false)!
+//
+//        NSURLConnection.sendAsynchronousRequest(urlRequest,
+//            queue: NSOperationQueue.mainQueue(),
+//            completionHandler: response)
+        
+        let manager = AFHTTPRequestOperationManager()
+        manager.requestSerializer = AFHTTPRequestSerializer()
+        manager.GET("http://express.heartrails.com/api/json?method=getPrefectures", parameters: nil,
+            success: {(operation: AFHTTPRequestOperation!, responsobject: AnyObject!) in
+                println("Success!!")
+                println(responsobject)
+            },
+            failure: {(operation: AFHTTPRequestOperation!, error: NSError!) in
+                println("Error!!")
+            }
+        )
     }
 
     func response(res: NSURLResponse!, data: NSData!, error: NSError!){
